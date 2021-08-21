ae6ac608e4675d2a081327617e34671f6e95fd7f
Carlo G <41587193+cleverguns@users.noreply.github.com>
2021-08-21 16:28:42 +0800

    Add files via upload

---
+++ minecraft_plugin.ts
@@ -0,0 +1,39 @@
+/**
+ * visit
+ * https://customrealms.io/
+ */
+
+import * as cr from 'your_server'; //mine is customrealms
+
+
+@cr.Plugin()
+export class MainPlugin implements cr.OnEnable
+{
+
+    /**
+     * 
+     * called when plguin is enabled
+     */
+    public onEnable(): void{
+
+    }
+
+    @cr.Command ('/hello')
+    public helloCommand(player: cr.Player){
+        player.SendTitle(
+            'Hello Dear Friends',
+            'from CustomRealms',
+            20.
+            60.
+            20
+
+        );
+    }
+    @cr.Command('/launch')
+    public launchCommand(player: cr.Player){
+        player.setVelocity(new cr.Vector(0, 100, 0))
+    }
+}
+
+//after this go to mincraft and type " /hello " w/o quotation
+//Cleverguns YT effort haha
